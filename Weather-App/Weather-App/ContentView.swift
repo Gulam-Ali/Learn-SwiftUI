b2f8b4f5b4725825a004da1b5cec19e2b227472a
Gulam Ali <gulam2275@gmail.com>
2023-11-13 22:56:21 +0530

    initial commit

---
+++ Weather-App/Weather-App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Weather-App
+//
+//  Created by Sachin Mittal on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            .background(Color.blue)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather-App/Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Sachin Mittal on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
